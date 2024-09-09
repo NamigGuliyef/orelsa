cb160f485accd20074f32be9718092f39ef77d7b
NamigGuliyef <quliyevnamiq8@gmail.com>
2024-09-09 11:34:04 +0400

    fix

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import dotenv from 'dotenv';
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
@@ -11,11 +13,12 @@ async function bootstrap() {
       'Orelsa - İran və digər ölkələrdən olan kosmetik məhsulların satışı',
     )
     .setVersion('1.0')
+    .addServer(process.env.BASE_URL || 'http://localhost:3000/api')
     .addBearerAuth()
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('', app, document);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(9089);
 }
