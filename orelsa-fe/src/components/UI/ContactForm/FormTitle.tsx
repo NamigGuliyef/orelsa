b044cda230aaf8805fb79b7a0fde6b604e78596a
quliyevnamiq8@gmail.com <quliyevnamiq8@gmail.com>
2024-12-10 23:41:42 +0400

    fix-1

--- orelsa-fe/src/components/UI/ContactForm/FormTitle.tsx
+++ orelsa-fe/src/components/UI/ContactForm/FormTitle.tsx
@@ -9,8 +9,8 @@ export const FormTitle = () => {
             <div className="mt-24 w-full flex  items-center flex-col gap-2">
               <h1 className="font-semibold text-4xl">Bizimlə Əlaqə Saxlayın</h1>
               <p className="text-base font-normal text-center">
-              "Məhsul və xidmətlərimiz haqqında daha ətraflı məlumat üçün bizə e-poçt göndərə bilərsiniz.
-               <br /> Komandamız sizə kömək etmək üçün həmişə hazırdır. Çəkinmədən bizimlə əlaqə saxlayın!"              
+              Məhsul və xidmətlərimiz haqqında daha ətraflı məlumat üçün bizə e-poçt göndərə bilərsiniz.
+               <br /> Komandamız sizə kömək etmək üçün həmişə hazırdır. Çəkinmədən bizimlə əlaqə saxlayın!            
               </p>
             </div>
           </section>
--- orelsa-fe/src/components/UI/Rooms/Rooms.tsx
+++ orelsa-fe/src/components/UI/Rooms/Rooms.tsx
@@ -14,9 +14,9 @@ const Rooms: React.FC = () => {
             "10+ Gözəllik və Baxım Sirri"
             </h3>
             <p className="w-[265px] pt-4 lg:pt-[105px]">
-             "Hər dəri və saç tipi üçün xüsusi hazırlanmış baxım sirrlərini kəşf edin! 
+             Hər dəri və saç tipi üçün xüsusi hazırlanmış baxım sirrlərini kəşf edin! 
               Gözəlliyinizə dəyər qatacaq təbii tərkibli kremlər və şampunlarla özünüzə zaman ayırın.
-              Sağlam və parlaq görünüş indi daha əlçatandır!"
+              Sağlam və parlaq görünüş indi daha əlçatandır!
             </p>
           </div>
           <div className="col-span-1 lg:col-span-3 lg:max-h-[556px] h-full">
