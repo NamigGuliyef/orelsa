5fc49d7565ac074d1713b56f50993fe3b8574a06
quliyevnamiq8@gmail.com <quliyevnamiq8@gmail.com>
2024-12-12 00:16:12 +0400

    fix 6

--- orelsa-back/src/main.ts
+++ orelsa-back/src/main.ts
@@ -3,7 +3,12 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin:"https://orelsa-fe.vercel.app",
+    methods: 'GET,POST,PUT,PATCH,DELETE', 
+    credentials: true,
+  })
 
   const config = new DocumentBuilder()
     .setTitle('ORELSA.AZ')
