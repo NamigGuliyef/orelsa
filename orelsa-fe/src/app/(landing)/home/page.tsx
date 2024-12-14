392b70b2d764949ea77a4eb9335ce380cd72868e
quliyevnamiq8@gmail.com <quliyevnamiq8@gmail.com>
2024-12-14 17:25:25 +0400

    kodlar geri qaytarildi

--- orelsa-fe/src/app/(landing)/home/page.tsx
+++ orelsa-fe/src/app/(landing)/home/page.tsx
@@ -1,44 +1,24 @@
-"use client";
-import { useEffect, useState } from "react";
 import HomePageImg from "@/../public/HomePage/HomePageImg.svg";
 import OurProducts from "@/components/UI/OurProducts/OurProducts";
 import Rooms from "@/components/UI/Rooms/Rooms";
 import TheRange from "@/components/UI/TheRange/TheRange";
 import Footer from "@/components/Views/Landing/Footer";
 import { Button } from "@nextui-org/react";
-import Link from "next/link";
 import axios from "axios";
 
-export default function HomePage() {
-  const [data, setData] = useState<any>([]);
-  const [loading, setLoading] = useState<boolean>(true);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get(
-          "http://localhost:9089/guest/homeNewCollection"
-        );
-        setData(response.data);
-        setLoading(false);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-        setLoading(false);
-      }
-    };
-
-    fetchData();
-  }, []); // Empty dependency array means this will run only once on component mount
+export default async function HomePage() {
+  const { data } = await axios.get(
+    "https://orelsa.vercel.app/guest/homeNewCollection"
+  );
 
   const backgroundImage = data[0]?.newproductPhoto
     ? `url(${data[0].newproductPhoto})`
     : `url(${HomePageImg.src})`;
-
   return (
     <>
       <main
         className="bg-no-repeat bg-cover h-screen flex justify-end items-center"
-        style={{ backgroundImage: backgroundImage }}
+        style={{ backgroundImage: `${backgroundImage}` }}
       >
         <div className="w-full md:w-[90%] lg:w-[60%] xl:w-[50%] shadow-2xl bg-white bg-opacity-40 rounded-[16px] px-6 py-8 mx-5">
           <h2 className="ml-0 md:ml-11 mt-4 font-bold text-[32px] sm:text-[42px] lg:text-[52px] text-[#B88E2F]">
@@ -48,13 +28,9 @@ export default function HomePage() {
             {data[0]?.description ?? "Saç gücləndirici"}
           </p>
           <div className="ml-0 md:ml-11 pb-6 sm:pb-10 pt-6 sm:pt-9">
-            <Link
-              href="https://wa.me/+994559706747"
-              target="_blank"
-              className="gap-3 bg-[#B88E2F] absolute w-full sm:w-[222px] h-[60px] sm:h-[75px] font-bold text-base text-white"
-            >
-              <p className="text-center my-7">İndi sifariş et</p>
-            </Link>
+            <Button className="gap-3 bg-[#B88E2F] w-full sm:w-[222px] h-[60px] sm:h-[75px] font-bold text-base text-white">
+              İndi sifariş et
+            </Button>
           </div>
         </div>
       </main>
