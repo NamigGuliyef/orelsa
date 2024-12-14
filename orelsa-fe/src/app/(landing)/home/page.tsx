fbf55c08189bcd5d9f4fbef315531f4157a4ab15
Alion-GreenHeart <56336368+Ali-GreenHeart@users.noreply.github.com>
2024-12-14 14:10:03 +0400

    Update page.tsx

--- orelsa-fe/src/app/(landing)/home/page.tsx
+++ orelsa-fe/src/app/(landing)/home/page.tsx
@@ -1,32 +1,34 @@
 "use client";
-
+import { useEffect, useState } from "react";
 import HomePageImg from "@/../public/HomePage/HomePageImg.svg";
 import OurProducts from "@/components/UI/OurProducts/OurProducts";
 import Rooms from "@/components/UI/Rooms/Rooms";
 import TheRange from "@/components/UI/TheRange/TheRange";
 import Footer from "@/components/Views/Landing/Footer";
 import { Button } from "@nextui-org/react";
-import { useEffect, useState } from "react";
+import Link from "next/link";
 import axios from "axios";
 
 export default function HomePage() {
-  const [data, setData] = useState<any[]>([]); // Məlumatları saxlamaq üçün state
+  const [data, setData] = useState<any>([]);
+  const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
-    // Məlumatları dinamik olaraq backend-dən alırıq
-    const fetchHomeData = async () => {
+    const fetchData = async () => {
       try {
         const response = await axios.get(
-          "https://orelsa.vercel.app/guest/homeNewCollection"
+          "http://localhost:9089/guest/homeNewCollection"
         );
         setData(response.data);
+        setLoading(false);
       } catch (error) {
         console.error("Error fetching data:", error);
+        setLoading(false);
       }
     };
 
-    fetchHomeData();
-  }, []); // useEffect yalnız komponent ilk dəfə mount olduqda işləyəcək
+    fetchData();
+  }, []); // Empty dependency array means this will run only once on component mount
 
   const backgroundImage = data[0]?.newproductPhoto
     ? `url(${data[0].newproductPhoto})`
@@ -46,9 +48,13 @@ export default function HomePage() {
             {data[0]?.description ?? "Saç gücləndirici"}
           </p>
           <div className="ml-0 md:ml-11 pb-6 sm:pb-10 pt-6 sm:pt-9">
-            <Button className="gap-3 bg-[#B88E2F] w-full sm:w-[222px] h-[60px] sm:h-[75px] font-bold text-base text-white">
-              İndi sifariş et
-            </Button>
+            <Link
+              href="https://wa.me/+994559706747"
+              target="_blank"
+              className="gap-3 bg-[#B88E2F] absolute w-full sm:w-[222px] h-[60px] sm:h-[75px] font-bold text-base text-white"
+            >
+              <p className="text-center my-7">İndi sifariş et</p>
+            </Link>
           </div>
         </div>
       </main>
